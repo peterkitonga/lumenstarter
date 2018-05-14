a5da2b652a809ee5d2ff6ca91af8ccd2edfb82a7
Peter Kitonga <pkitonga.pk@gmail.com>
2018-05-14 11:03:50 +0300

    Added map function the the roles response

--- app/Http/Controllers/V1/RolesController.php
+++ app/Http/Controllers/V1/RolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V1;
 
 use App\Role;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -24,7 +25,15 @@ class RolesController extends Controller
     {
         try {
             // Get a list of role records and parse them as an array
-            $roles = Role::query()->get()->toArray();
+            $roles = Role::query()->get()->map(function ($item) {
+                return [
+                    'id' => (int) $item['id'],
+                    'name' => (string) $item['role_name'],
+                    'slug' => (string) $item['role_slug'],
+                    'permissions' => (array) $item['role_permissions'],
+                    'date_added' => (string) Carbon::parse($item['created_at'])->format('j M Y h:i A'),
+                ];
+            })->toArray();
 
             return response()->json(['status' => 'success', 'data' => $roles]);
         } catch(\Exception $exception) {
