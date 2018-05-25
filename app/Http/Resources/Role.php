c20ac290b314498e02d9c0b7b239490fba0fb021
Peter Kitonga <pkitonga.pk@gmail.com>
2018-05-25 19:07:46 +0300

    Added an API resource for the roles responses

---
+++ app/Http/Resources/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\Resource;
+
+class Role extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (int) $this->id,
+            'name' => (string) $this->role_name,
+            'permissions' => (array) $this->role_permissions,
+            'date_added' => (string) Carbon::parse($this->created_at)->format('j M Y h:i A')
+        ];
+    }
+}
