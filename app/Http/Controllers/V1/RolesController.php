ecff7e9c3a082865a7d2da8ec1cf2840d5b3ebcb
Peter Kitonga <pkitonga.pk@gmail.com>
2018-05-25 19:40:04 +0300

    Applied the roles collection to the roles response

--- app/Http/Controllers/V1/RolesController.php
+++ app/Http/Controllers/V1/RolesController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers\V1;
 
+use App\Http\Resources\RoleCollection;
 use App\Role;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -21,21 +21,20 @@ class RolesController extends Controller
         $this->middleware('access.role:admin');
     }
 
+    /**
+     * Displays a list of records.
+     *
+     * @return RoleCollection|\Illuminate\Http\JsonResponse
+     */
     public function index()
     {
         try {
             // Get a list of role records and parse them as an array
-            $roles = Role::query()->get()->map(function ($item) {
-                return [
-                    'id' => (int) $item['id'],
-                    'name' => (string) $item['role_name'],
-                    'slug' => (string) $item['role_slug'],
-                    'permissions' => (array) $item['role_permissions'],
-                    'date_added' => (string) Carbon::parse($item['created_at'])->format('j M Y h:i A'),
-                ];
-            })->toArray();
+            $roles = Role::query()->get();
 
-            return response()->json(['status' => 'success', 'data' => $roles]);
+            $response = new RoleCollection($roles);
+
+            return $response;
         } catch(\Exception $exception) {
             return response()->json(['status' => 'error', 'message' => $exception->getMessage()]);
         }
