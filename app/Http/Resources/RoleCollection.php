d24852b9ce190f25ebedbe5f6d10857a83beb465
Peter Kitonga <pkitonga.pk@gmail.com>
2018-05-25 19:39:35 +0300

    Added an api role collection resource class

---
+++ app/Http/Resources/RoleCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class RoleCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'status' => 'success',
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value',
+            ],
+        ];
+    }
+}
