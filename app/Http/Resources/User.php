9030411a5995df3c00f3f781d123fedb9e5f527f
Peter Kitonga <pkitonga.pk@gmail.com>
2018-05-25 19:08:42 +0300

    Applied the role resource to the user role relationship in the user resource

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\Resource;
+use App\Http\Resources\Role as RoleResource;
 
 class User extends Resource
 {
@@ -23,7 +24,7 @@ class User extends Resource
             'is_active' => (bool) ($this->activation_status == 0 ? false : true),
             'is_deactivated' => (bool) ($this->deleted_at == null ? false : true),
             'image' => (string) $this->profile_image == null ? null : $this->profile_image,
-            'role' => (array) isset($this->roles[0]) ? $this->roles[0] : [],
+            'role' => (array) isset($this->roles[0]) ? new RoleResource($this->roles[0]) : [],
             'date_added' => (string) Carbon::parse($this->created_at)->format('j M Y h:i A'),
             'last_seen' => (string) $this->last_seen == null ? 'Never' : Carbon::parse($this->last_seen)->format('j M Y h:i A')
         ];
