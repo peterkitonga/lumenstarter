ce206f86832b759b9596e75ae526bf1918c1dbac
PeterKitonga <pkitonga.pk@gmail.com>
2018-06-04 12:27:52 +0300

    Removed unnecessary columns from the password resets table

--- database/migrations/2018_05_22_093723_create_password_resets_table.php
+++ database/migrations/2018_05_22_093723_create_password_resets_table.php
@@ -14,10 +14,9 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->increments('id');
             $table->string('email')->index();
             $table->string('token', 64)->index();
-            $table->timestamps();
+            $table->timestamp('created_at')->nullable();
         });
     }
 
