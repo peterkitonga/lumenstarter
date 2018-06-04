6875c2c779c7517c6454a0d777dba78c7fda1b6b
PeterKitonga <pkitonga.pk@gmail.com>
2018-06-04 12:16:12 +0300

    Added missing helper functions 'public_path' and 'asset'

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -10,6 +10,33 @@ if ( ! function_exists('config_path'))
      */
     function config_path($path = '')
     {
-        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+        return app()->basePath().'/config'.($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
+    }
+}
+
+if (! function_exists('public_path')) {
+    /**
+     * Get the path to the public folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function public_path($path = '')
+    {
+        return app()->basePath().'/public'.($path ? DIRECTORY_SEPARATOR.ltrim($path, DIRECTORY_SEPARATOR) : $path);
+    }
+}
+
+if (! function_exists('asset')) {
+    /**
+     * Generate an asset path for the application.
+     *
+     * @param  string  $path
+     * @param  bool    $secure
+     * @return string
+     */
+    function asset($path, $secure = null)
+    {
+        return app('url')->asset($path, $secure);
     }
 }
