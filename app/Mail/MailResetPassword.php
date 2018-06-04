c21d9d72a882ccf28d905d87cd9d98768a6f3e2a
PeterKitonga <pkitonga.pk@gmail.com>
2018-06-04 14:20:32 +0300

    Added a mailable class for reset password email

---
+++ app/Mail/MailResetPassword.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class MailResetPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     * @param $token
+     */
+    public function __construct(User $user, $token)
+    {
+        $this->user = $user;
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS'))
+            ->subject(env('MAIL_FROM_NAME').' - Reset Password')
+            ->view('emails.reset')
+            ->with([
+                'name' => $this->user->name,
+                'email' => $this->user->email,
+                'token' => $this->token
+            ]);
+    }
+}
