90b143287ef932566a8b6a0b843f455282fbd4f5
PeterKitonga <pkitonga.pk@gmail.com>
2018-06-18 11:35:52 +0300

    Minor update

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -28,8 +28,10 @@ class CheckAccess
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @param $role
+     * @param null $guard
      * @return mixed
      */
     public function handle($request, Closure $next, $role, $guard = null)
