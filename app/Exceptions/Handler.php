f4f0b82b26b1189013a4e36b39ab719e98652825
Peter <Kitonga>
2020-03-13 15:37:07 +0300

    Modified the exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,17 @@
 
 namespace App\Exceptions;
 
+use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -40,27 +45,57 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param Exception $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $e)
+    public function render($request, Exception $exception)
     {
-        // Handles the JWT exceptions
-        if ($e instanceof \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException) {
-            switch (get_class($e->getPrevious())) {
-                case \Tymon\JWTAuth\Exceptions\TokenExpiredException::class:
-                    return response()->json(['status' => 'error', 'message' => 'Token has expired'], $e->getStatusCode());
-                case \Tymon\JWTAuth\Exceptions\TokenInvalidException::class:
-                    return response()->json(['status' => 'error', 'message' => 'Token is invalid'], $e->getStatusCode());
-                case \Tymon\JWTAuth\Exceptions\TokenBlacklistedException::class:
-                    return response()->json(['status' => 'error', 'message' => 'Token is blacklisted'], $e->getStatusCode());
-                default:
-                    return response()->json(['status' => 'error', 'message' => 'Token not found'], 401);
-                    break;
+        // Handles the exceptions
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            $httpStatusCode = Response::HTTP_METHOD_NOT_ALLOWED;
+            $message = Response::$statusTexts[$httpStatusCode];
+        } elseif ($exception instanceof NotFoundHttpException) {
+            $httpStatusCode = Response::HTTP_NOT_FOUND;
+            $message = Response::$statusTexts[$httpStatusCode];
+        } elseif ($exception instanceof AuthorizationException) {
+            $httpStatusCode = Response::HTTP_UNAUTHORIZED;
+            $message = Response::$statusTexts[$httpStatusCode];
+        } elseif ($exception instanceof \Dotenv\Exception\ValidationException && $exception->getResponse()) {
+            $httpStatusCode = Response::HTTP_BAD_REQUEST;
+            $message = Response::$statusTexts[$httpStatusCode];
+        } elseif ($exception instanceof UnauthorizedHttpException) {
+            $httpStatusCode = Response::HTTP_UNAUTHORIZED;
+
+            if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
+                $message = 'Authorization token is Invalid';
+            } else if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
+                $message = 'Authorization token is Expired';
+            } else if ($exception instanceof \Tymon\JWTAuth\Exceptions\TokenBlacklistedException) {
+                $message = 'Authorization token is Blacklisted';
+            } else {
+                $message = 'Authorization token not found';
             }
+        } else {
+            $httpStatusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+            $message = Response::$statusTexts[$httpStatusCode];
         }
 
-        return parent::render($request, $e);
+        $exceptionLog = [
+            'path' => $request->path(),
+            'method' => $request->method(),
+            'request' => json_encode($request->toArray()),
+            'message' => empty($exception->getMessage()) ? $message : $exception->getMessage(),
+            'line' => $exception->getLine(),
+            'file' => $exception->getFile(),
+            'trace' => $exception->getTraceAsString()
+        ];
+
+        \Illuminate\Support\Facades\Log::error(json_encode($exceptionLog, true));
+
+        return response()->json([
+            'status'  => 'error',
+            'message' => $message
+        ], $httpStatusCode);
     }
 }
